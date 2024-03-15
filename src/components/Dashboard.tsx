10ac5d8c93506d1031783f8af5ebb62c064f6bb0
Megan Hugo <mhugo@dvtsoftware.com>
2024-03-15 14:03:34 +0200

    feat: :sparkles: Added Dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+
+const Dashboard = () => {
+    return (
+        <>
+            <div className="h-screen width-screen bg-black">
+
+            </div>
+        
+        </>
+    )
+}
+
+export default Dashboard;
