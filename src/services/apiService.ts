38dda5b1c16153beeda2ffd2864f6b920bcdabd7
Megan Hugo <mhugo@dvtsoftware.com>
2024-03-20 07:23:18 +0200

    feat: :sparkles: Added data manipulation for getCommits endpoint

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,7 @@
-import { IRepositoryResponse, IOrganisationResponse, ICommitResponse } from '../models'
+import { IRepositoryResponse, IOrganisationResponse, ICommitResponse, ICommit } from '../models'
 
 const username: string = 'aaronabramov'; // will dynamically change the user later (fron state)
-const baseUrl: string = `https://api.github.com`;
+const baseUrl: string = `https://api.github.com/`;
 
 export function getRepos(): void {
   fetch(baseUrl + `/users/${username}/repos`)
@@ -41,18 +41,37 @@ export function getOrgs(): Promise<IOrganisationResponse> {
     //need to get the url from getOrgs' response
   }
 
-  export function getCommits(repo: string, owner: string): void {
-    fetch(baseUrl + `repos/${owner}/${repo}/commits`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network error');
-        }
-        return response.json() as Promise<ICommitResponse[]>;
-      })
-      .then(data => {
-        console.log('Commits:', data);
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+export async function getCommits(owner: string, repo: string): Promise<ICommit[]> {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits?per_page=100`);
+    const commitData: ICommitResponse[] = await response.json();
+  
+    const allCommits: ICommit[] = commitData?.map(commitData => {
+      let commitType: 'initial' | 'merge' | 'commit' | 'branch';
+      switch (commitData.parents.length) {
+        case 0:
+          commitType = 'initial';
+          break;
+        case 1:
+          commitType = 'commit';
+          break;
+        case 2:
+          commitType = 'merge';
+          break;
+        default:
+          commitType = 'branch';
+          break;
+      }
+
+      const dataValue: ICommit = {
+        author: commitData.commit.author,
+        message: commitData.commit.message,
+        branches: commitData.parents.map(parent => parent.sha),
+        date: commitData.commit.author?.date ? commitData.commit.author.date.substring(0,10) : "Unknown",
+        type: commitType,
+        id: commitData.sha.substring(0, 7)
+      };
+      return dataValue;
+    });
+  
+    return allCommits;
   }
