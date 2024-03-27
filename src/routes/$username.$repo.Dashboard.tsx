5ab8f8a58998895931ced749403f2079df0b1c69
Megan Hugo <mhugo@dvtsoftware.com>
2024-03-27 16:33:02 +0200

    feat: :sparkles: Added routing for navbar

--- src/routes/$username.$repo.Dashboard.tsx
+++ src/routes/$username.$repo.Dashboard.tsx
@@ -1,6 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { Outlet, createFileRoute } from '@tanstack/react-router';
 import Dashboard from '../components/Dashboard';
+import Navbar from '../components/Navbar';
 
-export const Route = createFileRoute('/$username/$repo/Dashboard')({
-  component: Dashboard,
+export const Route = createFileRoute('/$username/$repo/dashboard')({
+  component: () => (
+    <>
+    <Navbar />
+    <div className="bg-gray-950 flex justify-center items-center pt-6">
+      <Outlet />
+    </div>
+    </>
+  ),
 });
---
+++ src/routes/$username.$repo.dashboard.git-tree.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/$username/$repo/dashboard/git-tree')({
+  component: () => <div>Hello /$username/$repo/dashboard/git-tree!</div>
+})
