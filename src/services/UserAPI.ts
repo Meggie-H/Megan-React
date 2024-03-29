1af5c8b82aa0167bebfe2e9d88095a5290edf366
Megan Hugo <mhugo@dvtsoftware.com>
2024-03-29 15:16:31 +0200

    style: :art: Separated API concerns

---
+++ src/services/UserAPI.ts
@@ -0,0 +1,17 @@
+import { octokit } from "../../environments/apiKey";
+import { IUserSearchResponse } from "../models";
+
+const baseUrl: string = `https://api.github.com/`;
+
+export async function getUser(username: string): Promise<IUserSearchResponse> {
+  try {
+    const response = await octokit.request(`${baseUrl}users/${username}`);
+    if (response.status !== 200) {
+      throw new Error(`Failed to fetch user data for ${username}`);
+    }
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+}
