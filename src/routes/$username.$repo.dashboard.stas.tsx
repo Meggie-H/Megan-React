02f7ee32d4052e91d79381ec637352b30c368ffb
Megan Hugo <mhugo@dvtsoftware.com>
2024-03-31 18:19:46 +0200

    feat: :sparkles: Added Dashboard routing

---
+++ src/routes/$username.$repo.dashboard.stas.tsx
@@ -0,0 +1,13 @@
+import { Outlet, createFileRoute } from '@tanstack/react-router'
+import Navbar from '../components/Navbar';
+
+export const Route = createFileRoute('/$username/$repo/dashboard')({
+  component: () => (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center bg-gray-950 pt-6">
+        <Outlet />
+      </div>
+    </>
+  ),
+});
